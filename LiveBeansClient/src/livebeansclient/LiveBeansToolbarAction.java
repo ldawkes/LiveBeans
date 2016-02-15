4cbc9ddc18826107d5d239fcb08f675d832be7ed
Luke Dawkes <ooddluke@gmail.com>
2016-02-15 18:21:20 +0000

    Toolbar Integration & Basic Connection Dialog
    The plugin now adds itself to NetBean's toolbar, using an icon from
    Icons8.com.
    
    The dialog box provides an area where the user can enter their name &
    the server's ip address.
    
    TODO: Add IP regex check, test & pass RMI connection back to main client
    class, get current working files / project to synchronise

---
+++ LiveBeansClient/src/livebeansclient/LiveBeansToolbarAction.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livebeansclient;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Bugtracking",
+        id = "livebeansclient.LiveBeansToolbarAction"
+)
+@ActionRegistration(
+        iconBase = "livebeansclient/sprites/LiveBeans_Connect_16.png",
+        displayName = "#CTL_LiveBeansToolbarAction"
+)
+@ActionReference(path = "Toolbars/File", position = 500)
+@Messages("CTL_LiveBeansToolbarAction=Connect to LiveBeans server")
+public final class LiveBeansToolbarAction implements ActionListener {
+
+    private ConnectionDialog _connectionDialog;
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (_connectionDialog == null) {
+            _connectionDialog = new ConnectionDialog(new JFrame(), true);
+            _connectionDialog.setLocationRelativeTo(null);
+        }
+        
+        _connectionDialog.setVisible(true);
+    }
+}
