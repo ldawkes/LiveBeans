a5c4fe89ee92be4df0347858662e49a5f2ad1504
Luke Dawkes <ooddluke@gmail.com>
2016-02-15 16:34:08 +0000

    Changed Client Project Type
    Changed project type of client from Java Application to NetBeans module
    for IDE integration

--- LiveBeansClient/src/livebeansclient/LiveBeansClient.java
+++ LiveBeansClient/src/livebeansclient/LiveBeansClient.java
@@ -10,12 +10,5 @@ package livebeansclient;
  * @author ooddl
  */
 public class LiveBeansClient {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
